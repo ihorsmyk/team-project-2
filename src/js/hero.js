69882beaf9aba4c96f85a14da7295254770bd1bc
Serhii Manko <msa01051984@gmail.com>
2022-12-07 20:07:33 +0200

    hero-update

---
+++ src/js/hero.js
@@ -0,0 +1,10 @@
+const refs = {
+    options: document.querySelector('.js-hero-mobile'),
+    items: document.querySelector('.js-hero-desktop')
+}
+
+// for (let i = 0; i < 9; i++) {
+//     const element = i;
+//     console.log(element)
+// }
+console.log(325)
