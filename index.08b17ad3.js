90b9ea22a4ea7f8ac4a1bdb763b561e925794dcd
IhorSmyk <112649980+IhorSmyk@users.noreply.github.com>
2022-12-07 18:15:20 +0000

    Deploying to gh-pages from @ IhorSmyk/team-project-2@514686f3f6eaa884ef1556c7993cc5efd08941f8 🚀

---
+++ index.08b17ad3.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".js-hero-mobile"),document.querySelector(".js-hero-desktop");console.log(325)}();
+//# sourceMappingURL=index.08b17ad3.js.map
---
+++ index.566d6f80.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-hero-mobile"),document.querySelector(".js-hero-desktop");console.log(325);
+//# sourceMappingURL=index.566d6f80.js.map
