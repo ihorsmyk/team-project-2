2f4a31ecc933c9e7b56e3f97b5eb5c5a5c9a74a6
Serhii Manko <msa01051984@gmail.com>
2022-12-09 23:41:03 +0200

    hero-update

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,10 +1,97 @@
+import { fetchCocktail } from './heroAPI.js';
+
 const refs = {
-    options: document.querySelector('.js-hero-mobile'),
-    items: document.querySelector('.js-hero-desktop')
+  formSelect: document.querySelector('.js-hero-mobile'),
+  formItems: document.querySelector('.js-hero-desktop'),
+};
+// console.log(refs.formSelect);
+
+const array = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+];
+
+let markup = [];
+
+function renderLitery(array) {
+  markup = array
+    .map(element => {
+      // console.log(element)
+      return `<option value="${element}">${element}</option>`;
+    })
+    .join('');
 }
 
-// for (let i = 0; i < 9; i++) {
-//     const element = i;
-//     console.log(element)
-// }
-console.log(325)
+renderLitery(array);
+
+refs.formSelect.insertAdjacentHTML('beforeend', markup);
+
+function renderItemsLitery(array) {
+  markupItems = array
+    .map(element => {
+      return `<li class="hero__name-items list">${element}</li>`;
+    })
+    .join('');
+}
+
+renderItemsLitery(array);
+refs.formItems.insertAdjacentHTML('beforeend', markupItems);
+
+refs.formItems.addEventListener('click', onTargetCocktailClick);
+
+function onTargetCocktailClick(event) {
+  event.preventDefault();
+  const nameCocktail = event.target.textContent;
+  // console.log(nameCocktail);
+  fetchCocktail(nameCocktail).then(data => {
+    console.log(data);
+  });
+}
+
+refs.formSelect.addEventListener('chanche', onTargetSearchCocktailClick);
+
+function onTargetSearchCocktailClick(event) {
+  event.preventDefault();
+
+  const nameSearchCoctail = event.target.value;
+
+  console.log(nameSearchCoctail);
+
+  fetchCocktail(nameSearchCoctail).then(data => {
+    console.log(data);
+  });
+}
